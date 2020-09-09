a03baaadf3cc9abfacbe1e032e6387d30545ab70
Diego Centurion <diego@topdek.com.br>
2020-09-09 14:48:24 -0400

    style added

--- appclient/settings.py
+++ appclient/settings.py
@@ -159,13 +159,13 @@ SITE_ID = 1
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'appclient/static/')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/images/'
